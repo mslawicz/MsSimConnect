9ae421232e2451f538d68e3bb3b1aa210cc74836
Marcin Sławicz <marcin.slawicz@gmail.com>
2020-09-20 10:45:17 +0200

    keyboard hit action added

--- MsSimConnect.cpp
+++ MsSimConnect.cpp
@@ -2,22 +2,23 @@
 //
 
 #include <Windows.h>
+#include <conio.h>
 #include "SimConnect.h"
 #include <iostream>
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "MS SimConnect v1.0\n";
+    std::cout << "press Q to quit\n";
+
+    int key = 0;
+    do
+    {
+        if (_kbhit())
+        {
+            key = toupper(_getch());
+            std::cout << std::hex << key << ",";
+        }
+    } while(key != 0x51);   //'Q'
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
