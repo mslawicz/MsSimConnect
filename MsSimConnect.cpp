d4b0049642bf36ed2a1d3ba337ac1be6242d1423
Marcin Sławicz <marcin.slawicz@gmail.com>
2021-08-10 16:32:22 +0200

    joystick thread enabled

--- MsSimConnect.cpp
+++ MsSimConnect.cpp
@@ -17,15 +17,15 @@ int main()
     Console::getInstance().log(LogLevel::Always, "MS SimConnect client v1.0");
     Console::getInstance().log(LogLevel::Always, "type 'help' for the list of commands");
 
-    //XXX test USBHID joystickLink(VENDOR_ID, PRODUCT_ID, REPORT_ID);
-    //XXX test Simulator::getInstance().setJoystickLink(&joystickLink);
-    //XXX test joystickLink.setParseFunction(std::bind(&Simulator::parseReceivedData, &Simulator::getInstance(), std::placeholders::_1));
+    USBHID joystickLink(VENDOR_ID, PRODUCT_ID, REPORT_ID);
+    Simulator::getInstance().setJoystickLink(&joystickLink);
+    joystickLink.setParseFunction(std::bind(&Simulator::parseReceivedData, &Simulator::getInstance(), std::placeholders::_1));
 
-    //XXX test std::thread joystickLinkThread(&USBHID::handler, &joystickLink);
+    std::thread joystickLinkThread(&USBHID::handler, &joystickLink);
     std::thread simulatorThread(&Simulator::handler, &Simulator::getInstance());
 
     Console::getInstance().handler();
 
     simulatorThread.join();
-    //XXX test joystickLinkThread.join();
+    joystickLinkThread.join();
 }
