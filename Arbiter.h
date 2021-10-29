10570854fd2f77a8185d40fe1be39f97cf305040
Marcin Sławicz <marcin.slawicz@gmail.com>
2021-10-29 18:40:48 +0200

    Arbiter class added

---
+++ Arbiter.h
@@ -0,0 +1,45 @@
+#pragma once
+
+template <class T>
+class Arbiter
+{
+public:
+    bool setRequested(T remote, T local, unsigned short counterLoad);
+private:
+    T lastRemote;
+    T lastLocal;
+    uint16_t remoteCounter;
+    uint16_t localCounter;
+};
+
+template <class T>
+bool Arbiter<T>::setRequested(T remote, T local, unsigned short counterLoad)
+{
+    bool setRequest{ false };
+
+    if ((remote != lastRemote) && (0 == localCounter))
+    {
+        remoteCounter = counterLoad;
+        setRequest = true;
+    }
+
+    if ((local != lastLocal) && (0 == remoteCounter))
+    {
+        localCounter = counterLoad;
+    }
+
+    lastRemote = remote;
+    lastLocal = local;
+
+    if (0 < remoteCounter)
+    {
+        remoteCounter--;
+    }
+
+    if (0 < localCounter)
+    {
+        localCounter--;
+    }
+
+    return setRequest;
+}
