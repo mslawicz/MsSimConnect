f1b5fcf6958696cf799e7f9293f814ecf965f432
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-06-28 22:43:17 +0200

    propeller pct scalling corrected

--- Simulator.cpp
+++ Simulator.cpp
@@ -68,7 +68,7 @@ void Simulator::handler(void)
             placeData<float>(static_cast<float>(simDataRead.rotationAccBodyZ), pBuffer);   // rotation acceleration body Z for yoke forces
             placeData<uint8_t>(static_cast<uint8_t>(simDataRead.engineType), pBuffer);     // enumerated engine type
             placeData<uint8_t>(scale<double, uint8_t>(0, 1.0, simDataCalculated.flapsPosPct, 0U, 0xFFU), pBuffer);  // percentage of flaps deflection <0,255>
-            placeData<uint8_t>(scale<double, uint8_t>(0, 1.0, simDataCalculated.propellerPct, 0U, 0xFFU), pBuffer);  // percentage of max propeller rpm <0,255>
+            placeData<uint8_t>(scale<double, uint8_t>(0, 100.0, simDataCalculated.propellerPct, 0U, 0xFFU), pBuffer);  // percentage of max propeller rpm <0,255>
             placeData<char>('S', pBuffer);
             placeData<char>('I', pBuffer);
             placeData<char>('M', pBuffer);
